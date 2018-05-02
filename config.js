bfae68154572545e8b8d95122aa80ec1762675df
Jacob <buckley.msu@gmail.com>
2018-05-01 23:19:46 -0700

    init

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
+var os = require("os");
+var hostname = os.hostname();
+
 module.exports = {
     'secret': 'FlippinDopeAssSecret',
-    'database': 'mongodb://127.0.0.1/dates',
+    'database': hostname === "FineAssComputer" ? 'mongodb://127.0.0.1/dates' : 'mongodb://dateabase:Westshaw1@ds111050.mlab.com:11050/heroku_fj618667',
     'uploads': '../uploads'
 };
