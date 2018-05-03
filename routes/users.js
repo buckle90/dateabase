fbfeab81333f3f95baebd32e9df228d8b00cb0df
Jacob <buckley.msu@gmail.com>
2018-05-02 21:27:45 -0700

    Uploads

--- routes/users.js
+++ routes/users.js
@@ -295,6 +295,34 @@ router.post('/upload', function (req, res) {
     });
 });
 
+router.get('/signS3', (req, res) => {
+    const s3 = new aws.S3();
+    const fileName = req.query['file-name'];
+    const fileType = req.query['file-type'];
+    const s3Params = {
+        Bucket: S3_BUCKET,
+        Key: fileName,
+        Expires: 60,
+        ContentType: fileType,
+        ACL: 'public-read'
+    };
+
+    s3.getSignedUrl('putObject', s3Params, (err, data) => {
+        if(err){
+            console.log(err);
+            return res.end();
+        }
+        const returnData = {
+            signedRequest: data,
+            url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+        };
+        console.log('ahhhh');
+        console.log(returnData);
+        res.write(JSON.stringify(returnData));
+        res.end();
+    });
+});
+
 async function signS3 (file, user) {
     const s3 = new aws.S3();
     const s3Params = {
